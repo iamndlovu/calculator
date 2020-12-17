48f80ee3db5f0e881e10af855634bde30a6b02fd
iamndlovu <ndlovu.p@protonmail.com>
2020-12-17 15:11:35 -0800

    handle click of operator buttons

--- js/data-handler.js
+++ js/data-handler.js
@@ -4,8 +4,6 @@ let current = '0',
 */
 let calculator = {
   state: {
-    firstNum: false,
-    solveClick: false,
     current: 'first'
   },
   data: {
@@ -16,18 +14,53 @@ let calculator = {
   },
 
   add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
   },
 
   subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
   },
 
   multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
   },
 
   divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
   },
+
+  solve() {
+    switch (this.data.operator) {
+      case 'divide':
+        this.data.result = this.divide(this.data.firstNum, this.data.secondNum);
+        break;
+      case 'subtract':
+        this.data.result = this.subtract(this.data.firstNum, this.data.secondNum);
+        break;
+      case 'multiply':
+        this.data.result = this.multiply(this.data.firstNum, this.data.secondNum);
+        break;
+      case 'add':
+      default:
+        this.data.result = this.add(this.data.firstNum, this.data.secondNum);
+    }
+    if (isNaN(this.data.result)) {
+      this.clear();
+      this.data.result = 'Error 2'
+    }
+  },
+
+  clear() {
+    this.state = {
+      current: 'first'
+    };
+    this.data = {
+      firstNum: '',
+      operator: '',
+      secondNum: '',
+      result: ''
+    };
+    answerSDisplay.textContent = '0';
+    problemDisplay.textContent = '0';
+  }
 };
--- js/index.js
+++ js/index.js
@@ -1,18 +1,47 @@
 const numberBtns = document.querySelector('#numbers'),
       problemDisplay = document.querySelector('.problem-display'),
-      answerSDisplay = document.querySelector('.answer-display');
+      answerSDisplay = document.querySelector('.answer-display'),
+      operators = document.querySelector('#operators');
 
 numberBtns.addEventListener('click', handleNumClick);
+operators.addEventListener('click', handleOpClick);
 
 function  handleNumClick(e) {
   let value = e.target.value;
   if (calculator.state.current == 'first') {
     calculator.data.firstNum += String(value);
-    calculator.state.firstNum = true;
     problemDisplay.textContent = calculator.data.firstNum;
-    console.log(calculator);
   } else {
     calculator.data.secondNum += String(value);
+    if (problemDisplay.textContent === '0') problemDisplay.textContent = ''
     problemDisplay.textContent = problemDisplay.textContent + String(value);
+    calculator.solve();
+    answerSDisplay.textContent = `= ${calculator.data.result}`;
+  }
+}
+
+function handleOpClick(e) {
+  if (e.target.value != 'solve') {
+    if (calculator.state.current == 'first') {
+      if (calculator.data.firstNum == '') calculator.data.firstNum = '0';
+    } else {
+      if (calculator.data.secondNum == '') calculator.data.secondNum = '0';
+      calculator.solve();
+      calculator.data.firstNum = calculator.data.result;
+      calculator.data.secondNum = '';
+    }
+    problemDisplay.textContent = problemDisplay.textContent + e.target.textContent;
+    calculator.data.operator = e.target.value;
+    calculator.state.current = 'second';
+  } else {
+    calculator.solve();
+    answerSDisplay.textContent = `= ${calculator.data.result}`;
+    calculator.state.current = 'first';
+    calculator.data = {
+      firstNum: '',
+      operator: '',
+      secondNum: '',
+      result: ''
+    }
   }
 }
